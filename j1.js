9d0fa57bc888f264cfaac491a7f2169927873ce2
athikahfatehah <65290497+athikahfatehah@users.noreply.github.com>
2020-05-14 00:17:59 +0800

    update j1.js

--- j1.js
+++ j1.js
@@ -1,6 +1,6 @@
 function validate(){
   var name = document.getElementById("name").value;
-  var subject = document.getElementById("subject").value;
+  
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
